93842792783ef9512898976b3eef6cd24164bf2a
Arjun Barrett <arjunbarrett@gmail.com>
2020-12-10 11:33:31 -0800

    Init

---
+++ src/index.ts
@@ -0,0 +1,91 @@
+import wk from './node-worker';
+
+type Context = [string, unknown, ArrayBuffer[]];
+type DepList = () => unknown[];
+
+const fnName = (f: Function, i: number) => {
+  if (f.name) return f.name;
+  const ts = f.toString();
+  return ts.slice(8, ts.indexOf('(', 8)) || ('_cls' + i);
+}
+
+const getAllPropertyKeys = (o: object) => {
+  let keys: (string | symbol)[] = Object.getOwnPropertyNames(o);
+  if (Object.getOwnPropertySymbols) {
+    keys = keys.concat(Object.getOwnPropertySymbols(o));
+  }
+  return keys;
+}
+
+const encoder = {
+  undefined: () => 'void 0',
+  bigint: (v: BigInt) => v.toString() + 'n',
+  symbol: (v: symbol) => {
+    const key = Symbol.keyFor(v);
+    return key
+      ? `Symbol.for(${encoder.string(key)})`
+      : `Symbol(${encoder.string(v.toString().slice(7, -1))})`
+  },
+  string: (v: string) => JSON.stringify(v),
+  function: (v: Function) => {
+    let st = v.toString();
+    if (v.prototype) {
+      // for global objects
+      if (st.indexOf('[native code]', 12) != -1)
+        st = st.slice(9, st.indexOf('(', 10))
+      else if (st[0] != 'c') {
+        // Not an ES6 class; must iterate across the properties
+        // Ignore superclass properties, assume superclass is handled elsewhere
+        st = '(function(){var v=' + st;
+        for (const t of getAllPropertyKeys(v.prototype)) {
+          const val = v.prototype[t];
+          if (t != 'constructor') {
+            st += `;v[${encoder[typeof t](t as never)}]=${encoder[typeof val](val as never)}`;
+          }
+        }
+        st += ';return v})()'
+      }
+    }
+    return st;
+  },
+  object: (v: object) => {
+    if (v == null) return 'null';
+    let out = '';
+    out += `(function(){`;
+    for (let i = 0, l = v; l.constructor != Object; l = Object.getPrototypeOf(l), ++i) {
+      const cls = l.constructor;
+      out += `self[${encoder.string(fnName(cls, i))}]=${encoder.function(cls)};`
+    }
+    out += `var v=Object.create(${fnName(v.constructor, 0)});`
+    for (const t of getAllPropertyKeys(v)) {
+      const { enumerable, configurable, get, set, writable, value } = Object.getOwnPropertyDescriptor(v, t);
+      let desc = '{';
+      if (typeof writable == 'boolean') {
+        desc += `writable:${writable},value:${encoder[typeof value](value as never)}`;
+      } else {
+        desc += `get:${get ? encoder.function(get) : 'void 0'},${set ? encoder.function(set) : 'void 0'}`
+      }
+      desc += `,enumerable:${enumerable},configurable:${configurable}}`;
+      out += `Object.defineProperty(v, ${encoder[typeof t](t as never)}, ${desc});`
+    }
+    return out + 'return v})()';
+  },
+  boolean: (v: boolean) => v.toString(),
+  number: (v: number) => v.toString(),
+};
+
+
+export function createContext(depList: DepList) {
+  const depListStr = depList.toString();
+  const depNames = depListStr.slice(
+    depListStr.indexOf('[') + 1,
+    depListStr.lastIndexOf(']')
+  ).replace(/\s/g, '').split(',');
+  const depValues = depList();
+  let out = '';
+  for (let i = 0; i < depValues.length; ++i) {
+    const key = depNames[i], value = depValues[i];
+    out += `self[${encoder.string(key)}]=${encoder[typeof value](value as never)}`;
+  }
+  return out;
+}
---
+++ src/node-worker.ts
@@ -0,0 +1,20 @@
+// Mediocre shim
+import { Worker } from 'worker_threads';
+
+const workerAdd = ";var __w=require('worker_threads');__w.parentPort.on('message',function(m){onmessage({data:m})}),postMessage=function(m,t){__w.parentPort.postMessage(m,t)},close=process.exit;self=global";
+
+export default <T>(c: string, msg: unknown, transfer: ArrayBuffer[], cb: (err: unknown, msg: T) => void) => {
+  let done = false;
+  const w = new Worker(c + workerAdd, { eval: true })
+    .on('error', e => cb(e, null))
+    .on('message', m => cb(null, m))
+    .on('exit', c => {
+      if (c && !done) cb(new Error('Exited with code ' + c), null);
+    });
+  w.postMessage(msg, transfer);
+  w.terminate = () => {
+    done = true;
+    return Worker.prototype.terminate.call(w);
+  }
+  return w;
+}
